fabca1892ded8aeaafdc91bcc9e21a9b084ccffd
tuedt <tuedt@fsoft.com.vn>
2017-11-10 17:03:13 +0700

    bookmark generator

--- src/main/java/com/twiki/pdf/BookmarksUtil.java
+++ src/main/java/com/twiki/pdf/BookmarksUtil.java
@@ -10,6 +10,8 @@ import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlin
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This is an example on how to add bookmarks to a PDF document.  It simply
@@ -50,6 +52,44 @@ public class BookmarksUtil {
 
             document.save(outFile);
         }
+    }
+
+    public static void generateBookmark(File inFile, File outFile, Map<Integer, String> bookmarks) throws IOException {
+        if (bookmarks == null) {
+            throw new IllegalArgumentException("Bookmarks cannot be null");
+        }
+        List<Integer> indexPages = bookmarks.keySet().stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        try (PDDocument document = PDDocument.load(inFile)) {
+            if (document.isEncrypted()) {
+                throw new IOException("Error: Cannot add bookmarks to encrypted document.");
+            }
+            PDDocumentOutline outline = new PDDocumentOutline();
+            document.getDocumentCatalog().setDocumentOutline(outline);
+            PDOutlineItem pagesOutline = new PDOutlineItem();
+            pagesOutline.setTitle("All Pages");
+            outline.addLast(pagesOutline);
+
+            List<PDPage> pages = Lists.newArrayList(document.getPages().iterator());
+            for (int i = 0; i < indexPages.size(); i++) {
+                int pageIndex = indexPages.get(i);
+                PDPage page = pages.get(pageIndex);
+                PDOutlineItem bookmark = makeBookMark(bookmarks.get(pageIndex), page);
+                pagesOutline.addLast(bookmark);
+            }
+            pagesOutline.openNode();
+            outline.openNode();
+
+            document.save(outFile);
+            document.close();
+        }
+    }
+
+    public Map<Integer, String> shiftBookMarks(Map<Integer, String> bookmarks, int offset) {
+//        bookmarks.entrySet().stream().
+        return bookmarks;
     }
 
     private static PDOutlineItem makeBookMark(String title, PDPage targetPage) {
--- src/main/java/com/twiki/tools/BookmarkGeneratorApp.java
+++ src/main/java/com/twiki/tools/BookmarkGeneratorApp.java
@@ -3,11 +3,16 @@ package com.twiki.tools;
 import com.twiki.pdf.BookmarksUtil;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookmarkGeneratorApp {
     public static void main(String[] args) throws Exception {
         File pdfIn = new File("D:\\books\\book_pdf\\build-apis-you-wont-hate\\03_useful-database-seeding.pdf");
         File pdfOut = new File("D:\\books\\book_pdf\\03_useful-database-seeding-withbookmarks.pdf");
-        BookmarksUtil.generateBookmark(pdfIn, pdfOut);
+        Map<Integer, String> bookmarks = new HashMap<>();
+        bookmarks.put(1, "Day la page 2");
+        bookmarks.put(4, "Day la page 5");
+        BookmarksUtil.generateBookmark(pdfIn, pdfOut, bookmarks);
     }
 }
